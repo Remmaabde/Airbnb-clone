acd4662be37570906e69ccb7bc515219b28829fa
Reema_abde <Reemabdella702@gmail.com>
2025-07-06 11:49:22 +0300

    Initialize Airbnb clone project with Vite + React

---
+++ Air-bnb/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar(){
+    return(
+        <nav className="navbar">
+            <img src="../img/Airbnb-Logo.wine.png" alt="airbnb logo" className="nav-logo" />
+        </nav>
+    )
+}
