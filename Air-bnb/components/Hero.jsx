413dcd102843e4a1e0adc7004dc3e4da8c6d6976
Reema_abde <Reemabdella702@gmail.com>
2025-07-06 12:13:43 +0300

    Added the hero section

---
+++ Air-bnb/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Hero(){
+    return(
+        <main>
+            <img src="../img/Group 77.png" alt="Images" />
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led <br /> by one-of-a-kind hosts—all without leaving <br /> home.
+            </p>
+        </main>
+    )
+}
--- Air-bnb/src/App.jsx
+++ Air-bnb/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 function App() {
   
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      
     </>
   )
 }
