ffbec156e41da242f570b7fdae01d8f489f7e741
Reema_abde <Reemabdella702@gmail.com>
2025-07-08 00:33:18 +0300

    Finalized Air bnb clone project

--- Air-bnb/components/Card.jsx
+++ Air-bnb/components/Card.jsx
@@ -1,13 +1,21 @@
 import React from "react"
 
 export default function Card(props){
+    let badgeText
+        if (props.openSpots === 0) {
+            badgeText = "SOLD OUT"
+        } else if (props.location === "Online") {
+            badgeText = "ONLINE"
+        }
     return(
+        
         <div className="card">
-            <img src={`../public/img/${props.img}`} alt="Kaite-zaferes" className="card-image" />
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../public/img/${props.coverImg}`} alt="Kaite-zaferes" className="card-image" />
             <div className="card-stats">
                 <img src="../img/Star 1.png" alt="Star" className="card-star" />
-                <span className="gray">{props.rating}</span>
-                <span className="gray">{props.points}</span>
+                <span className="gray">{props.stats.rating}</span>
+                <span className="gray">{props.stats.reviewCount}</span>
                 <span className="gray">{props.location}</span>
             </div>
             <h2>{props.lesson}</h2>
--- Air-bnb/src/App.jsx
+++ Air-bnb/src/App.jsx
@@ -8,13 +8,8 @@ import data from '../src/data'
 function App() {
   const cards = data.map(item => {
     return(
-      <Card img={item.coverImg}
-              rating={item.stats.rating}
-              points={item.stats.reviewCount}
-              location={item.location}
-              lesson={item.title}
-              price={item.price}
-              key={item.id}
+      <Card   key={item.id}
+              {...item}
       />
     )
   })
