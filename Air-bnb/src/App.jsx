c6a86b5db3782e4bab18ed631d939cd734c8cc48
Reema_abde <Reemabdella702@gmail.com>
2025-07-07 23:29:04 +0300

    Mapping components

--- Air-bnb/src/App.jsx
+++ Air-bnb/src/App.jsx
@@ -3,21 +3,26 @@ import './App.css'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Card from '../components/Card'
+import data from '../src/data'
 
 function App() {
-  
-
+  const cards = data.map(item => {
+    return(
+      <Card img={item.coverImg}
+              rating={item.stats.rating}
+              points={item.stats.reviewCount}
+              location={item.location}
+              lesson={item.title}
+              price={item.price}
+              key={item.id}
+      />
+    )
+  })
   return (
     <>
       <Navbar />
       <Hero />
-      <Card img="img/image 12.png"
-             rating={5.0}
-             points={6 }
-             country="Ethiopia"
-             lesson="Life lessons from katie zaferes"
-             price={136}
-        />
+      {cards}
       
     </>
   )
